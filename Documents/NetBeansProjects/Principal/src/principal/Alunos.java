6945c8861ea63f3f79278b7606ff0973acd8bc55
CesarGuilherme3110 <cieswrpalma@gmail.com>
2019-10-03 12:25:10 -0300

    Commit do exercicio ATUALIZADO

--- Documents/NetBeansProjects/Principal/src/principal/Alunos.java
+++ Documents/NetBeansProjects/Principal/src/principal/Alunos.java
@@ -1,14 +1,66 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package principal;
 
-/**
- *
- * @author Aluno01
- */
 public class Alunos {
     
+    private String Nome;
+    private int Cod;
+    private int nota1;
+    private int nota2;
+    private int nota3;
+    private int nota4;
+    
+    
+    public String getNome(){
+    return Nome;
+    }
+      public void setNome(String nome){
+      this.Nome = nome; 
+      }
+      
+      
+        public int getCod(){
+        return Cod;
+        }
+        public void setCod(int cod){
+        this.Cod = cod;
+        }
+           
+          public int getnota1(){
+          return nota1;
+          }  
+          public void setnota1(int nota1){
+           this.nota1 = nota1;   
+          }
+          
+          public int getnota2(){
+          return nota2;
+          }
+          public void setnota2 (int nota2){
+              this.nota2 = nota2; 
+          }
+
+           
+          public int getnota3(){
+          return nota3;
+          }
+          public void setnota3(int nota3){
+          this.nota3 = nota3;
+          }
+          
+          public int getnota4(){
+          return nota4;
+          }
+          public void setnota4(int nota4){
+          this.nota4 = nota4;
+          }
+          
+          
+          
+          public void exibirNome{
+
+
+}
+          
+          
 }
